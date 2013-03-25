8ec7018ca02b1470414d01a3c3cc526a1c5ddaa1
jcerjak <jcerjak@termitnjak.si>
2013-03-25 14:52:56 +0100

    #6165: save file fields into a separate table
    Added cleanString for cleaning data for DB insertion, transaction rollback on errors, small bugfixes.

--- src/slc/rdbploneformgenadapter/content/content.py
+++ src/slc/rdbploneformgenadapter/content/content.py
@@ -5,7 +5,7 @@ from Products.PloneFormGen.interfaces import IPloneFormGenActionAdapter
 from slc.rdbploneformgenadapter import SlcMessageFactory as _
 from slc.rdbploneformgenadapter.interfaces import \
     IRDBPloneFormGenAdapterContent
-from sqlalchemy.exc import ProgrammingError
+from sqlalchemy.exc import SQLAlchemyError
 from zope.component import getUtility, ComponentLookupError
 from zope.component.factory import Factory
 from zope.interface import implements
@@ -30,6 +30,23 @@ class RDBPloneFormGenAdapterContent(Item):
         pass
     title = property(_getProperty, _setProperty)
 
+    def _cleanString(self, string):
+        """Changes - to _ and removes dots"""
+        return(string.replace('-', '_').replace('.', ''))
+
+    def _concatenate(attributes, colon=False):
+        """Helper function for constructing SQL queries."""
+
+        # TODO think about setting column names manually
+        s = '('
+        if(colon):
+            s += ':'
+            tmp = ', :'.join(attributes)
+        else:
+            tmp = ', '.join(attributes)
+        s += tmp + ')'
+        return(s)
+
     def _constructQueryData(self, fields, form_table_name, REQUEST):
         """Parses form fields and prepares data for saving into the database.
 
@@ -37,23 +54,32 @@ class RDBPloneFormGenAdapterContent(Item):
         :param form_table_name: name of the table used for the form
         :param REQUEST: request object
         :returns: A list of tuples that contain two elements:
-            @tuple[0] is the name of the SQL table to save the data in
-            @tuple[1] is either a dict, or a list of dicts (for data grid
+            tuple[0] is the name of the SQL table to save the data in
+            tuple[1] is either a dict, or a list of dicts (for data grid
                 fields) with the values needed to execute the query
         """
         query_data = [(form_table_name, {})]
         query_args = query_data[0][1]
 
         for field in fields:
-            field_id = field.id.replace('-', '_')
+            field_id = self._cleanString(field.id)
 
             # handle file fields
             if field.isFileField():
                 field_name = field.fgField.getName()
-                file_upload = REQUEST.form.get(
-                    '{0}_file'.format(field_name.replace('-', '_')))
-                query_args[field_id + '_file'] = buffer(
-                    file_upload.blob.open().read())
+                file_upload = REQUEST.form.get('{0}_file'.format(field_name))
+                file_type = file_upload.filename.split('.')[-1]
+                table_name = form_table_name + '_' + \
+                    self._cleanString(field_name)
+                query_data.append((
+                    table_name,
+                    #XXX: check if it is possible that an object
+                    # has no read() method
+                    [{
+                        "data": buffer(file_upload.read()),
+                        "type": file_type
+                    }]
+                ))
 
             # handle datagrid fields
             elif field.portal_type == "FormDataGridField":
@@ -65,7 +91,8 @@ class RDBPloneFormGenAdapterContent(Item):
                         item = dict(row)
                         for key, value in item.iteritems():
                             item[key] = item[key].decode('utf8')
-                            item[key.replace('-', '_')] = item.pop(key)
+                            item[self._cleanString(key)] = \
+                                item.pop(key)
                         del item['orderindex_']
                         # check for duplicates
                         if(item not in grid_list):
@@ -85,35 +112,24 @@ class RDBPloneFormGenAdapterContent(Item):
         It works in the following way: if a query is defined on the adapter,
         it will use that query to insert the data into the database. If no
         query is defined, it will try to automatically save all form fields.
-        "Normal" fields will be saved into the primary table, while every
-        FormDataGridField field will be saved into a separate table.
+        "Normal" fields will be saved into the primary table, while
+        FormDataGridField and FormFileField fields will be saved into
+        separate tables.
 
         For this to work, your database and tables have to be created and set
         up according to the following rules:
           - Name of the primary table for storing the form data is same as
-            the id of the form (with '-' changed to '_')
-          - All "auxiliary" tables for FormDataGridField fields are named in
-            the convention of form_table_name + _ + FormDataGridField.id.
-            Also, each auxiliary table must have a form_table_name_id column,
-            which is a foreign key.
+            the id of the form (with '-' changed to '_' and '.' to '')
+          - All "auxiliary" tables for FormDataGridField and FormFileField
+            fields are named in the convention of
+            form_table_name + _ + field.id. Also, each auxiliary table must
+            have a form_table_name_id column, which is a foreign key.
           - All table colums must be named after form field id's (with '-'
-            changed to '_')
+            changed to '_' and '.' to '')
 
         :param fields: form fields
         :param REQUEST: request object
         """
-        def concatenate(attributes, colon=False):
-            """Helper function for constructing SQL queries."""
-
-            # TODO think about setting column names manually
-            s = '('
-            if(colon):
-                s += ':'
-                tmp = ', :'.join(attributes)
-            else:
-                tmp = ', '.join(attributes)
-            s += tmp + ')'
-            return(s)
 
         try:
             db = getUtility(IDatabase, self.db_utility_name)
@@ -125,26 +141,29 @@ class RDBPloneFormGenAdapterContent(Item):
                 )
             }
 
-        form_table_name = self.getParentNode().id.replace('-', '_')
+        form_table_name = self._cleanString(self.getParentNode().id)
         query_args = self._constructQueryData(
             fields, form_table_name, REQUEST)
         query = self.query
 
         try:
+            connection = db.connection.engine.connect()
+            # begin transaction for all executions
+            trans = connection.begin()
             if(query):
                 # custom query was defined on the adapter, let's use that
-                db.connection.engine.execute(query, **query_args[0][1])
+                connection.execute(query, **query_args[0][1])
             else:
                 # no query was defined, store all form fields
                 select_string = "SELECT MAX(ID) FROM {0}".format(
                     form_table_name)
-                form_id = (db.connection.engine.execute(
+                form_id = (connection.execute(
                     select_string).scalar() or 0) + 1
 
                 for field in query_args:
                     table_name = field[0]
 
-                    # handle FormDataGridField fields
+                    # insert FormDataGridField fields into separate tables
                     if(type(field[1]) == list):
                         for item in field[1][:]:
                             # Setting grid table name, based on the form name
@@ -153,27 +172,31 @@ class RDBPloneFormGenAdapterContent(Item):
                             query_string = \
                                 "INSERT INTO {0} {1} VALUES {2}".format(
                                     table_name,
-                                    concatenate(attributes),
-                                    concatenate(attributes, colon=True)
+                                    self._concatenate(attributes),
+                                    self._concatenate(attributes, colon=True)
                                 )
-                            db.connection.engine.execute(
-                                query_string, **item)
+                            connection.execute(query_string, **item)
+                    # insert "normal" fields into the primary table
                     else:
                         attributes = field[1].keys()
                         query_string = \
                             "INSERT INTO {0} {1} VALUES {2}".format(
                                 table_name,
-                                concatenate(attributes),
-                                concatenate(attributes, colon=True)
+                                self._concatenate(attributes),
+                                self._concatenate(attributes, colon=True)
                             )
-                        db.connection.engine.execute(
-                            query_string, **field[1])
-        except ProgrammingError, e:
+                        connection.execute(query_string, **field[1])
+            # commit the transaction, saving the changes
+            trans.commit()
+        except SQLAlchemyError, e:
+            trans.rollback()
             return {
                 FORM_ERROR_MARKER: _(
-                    'Can not write to database, wrong '
-                    'configuration. Please contact site owner.') + str(e)
+                    'Database not configured correctly '
+                    'Please contact site owner.') + str(e)
             }
+        except:
+            trans.rollback()
 
 
 RDBPloneFormGenAdapterFactory = Factory(
